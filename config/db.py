dc08e53b2e9a69619e9fe3abfeea1037cd116aa8
Gustavo <107815644+GukCM@users.noreply.github.com>
2022-07-26 19:47:17 -0500

    Delete db.py

--- config/db.py
+++
@@ -1,8 +0,0 @@
-from venv import create
-from sqlalchemy import create_engine, MetaData
-
-engine = create_engine("mysql+pymysql://root:root@localhost:3306/fastapidb")
-
-meta = MetaData()
-
-conn = engine.connect()
