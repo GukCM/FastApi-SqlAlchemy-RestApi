c5ed338550974366e92493b0403078eb7c110e70
Gustavo <107815644+GukCM@users.noreply.github.com>
2022-07-26 19:48:31 -0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-#Conda prompt conda create --name fastapi-mysql python=3
-#conda prompt conda activate fastapi-mysql
-#python -m uvicorn app:app --reload
-#-50 -330
-from fastapi import FastAPI
-from routes.user import user
-app = FastAPI()
-
-app.include_router(user)
